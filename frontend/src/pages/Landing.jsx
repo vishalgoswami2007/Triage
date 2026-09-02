ebfe538187a92148d4bd00629201ba8642d79ff0
Vishal Goswami <vg0767875@gmail.com>
2026-09-02 09:16:01 +0530

    feat: create landing page layout

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/landing/Navbar";
+import Hero from "../components/landing/Hero";
+
+function Landing() {
+  return (
+    <main className="min-h-screen bg-black">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+}
+
+export default Landing;
